b47de6a2bd72b366ae9e5613b2fde2d8a3ee82b7
thomas <thomas@vaadin.com>
2019-11-06 11:24:25 +0200

    changes version in about to platform instead of flow version

--- src/main/java/com/vaadin/samples/bookstore/ui/about/AboutView.java
+++ src/main/java/com/vaadin/samples/bookstore/ui/about/AboutView.java
@@ -1,12 +1,13 @@
 package com.vaadin.samples.bookstore.ui.about;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.Version;
 import com.vaadin.samples.bookstore.ui.MainLayout;
+import com.vaadin.shrinkwrap.VaadinCoreShrinkWrap;
 
 @Route(value = "About", layout = MainLayout.class)
 @PageTitle("About")
@@ -16,11 +17,15 @@ public class AboutView extends HorizontalLayout {
 
     public AboutView() {
         add(VaadinIcon.INFO_CIRCLE.create());
-        add(new Span(" This application is using Vaadin Flow "
-                + Version.getFullVersion() + "."));
+        add(new Span(
+                " This application is using Vaadin version "
+                        + VaadinCoreShrinkWrap.class
+                                .getAnnotation(NpmPackage.class).version()
+                        + "."));
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setAlignItems(Alignment.CENTER);
+
     }
 }
