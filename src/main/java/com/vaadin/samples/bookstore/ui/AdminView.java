ea27c5d959a78ba7032ea7639471b72a2bf4a275
thomas <thomas@vaadin.com>
2019-11-06 11:17:09 +0200

    adds confirmation when deleting categories

--- src/main/java/com/vaadin/samples/bookstore/ui/AdminView.java
+++ src/main/java/com/vaadin/samples/bookstore/ui/AdminView.java
@@ -1,12 +1,11 @@
 package com.vaadin.samples.bookstore.ui;
 
-import static com.vaadin.samples.bookstore.ui.AdminView.VIEW_NAME;
-
 import java.util.ArrayList;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -18,7 +17,6 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
-import com.vaadin.flow.router.Route;
 import com.vaadin.samples.bookstore.backend.DataService;
 import com.vaadin.samples.bookstore.backend.data.Category;
 
@@ -38,35 +36,54 @@ public class AdminView extends VerticalLayout {
     public AdminView() {
         categoriesListing = new IronList<>();
 
-        dataProvider = new ListDataProvider<Category>(new ArrayList<>(DataService.get().getAllCategories()));
+        dataProvider = new ListDataProvider<Category>(
+                new ArrayList<>(DataService.get().getAllCategories()));
         categoriesListing.setDataProvider(dataProvider);
-        categoriesListing.setRenderer(new ComponentRenderer<>(this::createCategoryEditor));
+        categoriesListing.setRenderer(
+                new ComponentRenderer<>(this::createCategoryEditor));
 
         newCategoryButton = new Button("Add New Category", event -> {
-            Category category = new Category();
+            final Category category = new Category();
             dataProvider.getItems().add(category);
             dataProvider.refreshAll();
         });
         newCategoryButton.setDisableOnClick(true);
 
-        add(new H2("Hello Admin"), new H4("Edit Categories"), newCategoryButton, categoriesListing);
+        add(new H2("Hello Admin"), new H4("Edit Categories"), newCategoryButton,
+                categoriesListing);
     }
 
     private Component createCategoryEditor(Category category) {
-        TextField nameField = new TextField();
+        final TextField nameField = new TextField();
         if (category.getId() < 0) {
             nameField.focus();
         }
 
-        Button deleteButton = new Button(VaadinIcon.MINUS_CIRCLE_O.create(), event -> {
-            DataService.get().deleteCategory(category.getId());
-            dataProvider.getItems().remove(category);
-            dataProvider.refreshAll();
-            Notification.show("Category Deleted.");
-        });
+        final Button deleteButton = new Button(
+                VaadinIcon.MINUS_CIRCLE_O.create(), event -> {
+
+                    // Ask for confirmation before deleting stuff
+                    final ConfirmDialog dialog = new ConfirmDialog(
+                            "Please confirm",
+                            "Are you sure you want to delete the category? Books in this category will not be deleted.",
+                            "Yes", e -> {
+                                DataService.get()
+                                        .deleteCategory(category.getId());
+                                dataProvider.getItems().remove(category);
+                                dataProvider.refreshAll();
+                                Notification.show("Category Deleted.");
+                            });
+
+                    dialog.setCancelable(true);
+
+                    add(dialog);
+                    dialog.open();
+
+                });
         deleteButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
 
-        BeanValidationBinder<Category> binder = new BeanValidationBinder<>(Category.class);
+        final BeanValidationBinder<Category> binder = new BeanValidationBinder<>(
+                Category.class);
         binder.forField(nameField).bind("name");
         binder.setBean(category);
         binder.addValueChangeListener(event -> {
@@ -79,7 +96,8 @@ public class AdminView extends VerticalLayout {
         });
         deleteButton.setEnabled(category.getId() > 0);
 
-        HorizontalLayout layout = new HorizontalLayout(nameField, deleteButton);
+        final HorizontalLayout layout = new HorizontalLayout(nameField,
+                deleteButton);
         layout.setFlexGrow(1);
         return layout;
     }
