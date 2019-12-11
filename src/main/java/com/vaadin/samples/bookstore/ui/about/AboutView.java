5b586efd4a6ef50270e2e2df83dc62fda4a95b01
Manuel Carrasco Moñino <manolo@vaadin.com>
2019-12-11 09:59:59 +0200

    fix build when using platform snapshots (#14)

--- src/main/java/com/vaadin/samples/bookstore/ui/about/AboutView.java
+++ src/main/java/com/vaadin/samples/bookstore/ui/about/AboutView.java
@@ -1,13 +1,12 @@
 package com.vaadin.samples.bookstore.ui.about;
 
-import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.Version;
 import com.vaadin.samples.bookstore.ui.MainLayout;
-import com.vaadin.shrinkwrap.VaadinCoreShrinkWrap;
 
 @Route(value = "About", layout = MainLayout.class)
 @PageTitle("About")
@@ -17,12 +16,8 @@ public class AboutView extends HorizontalLayout {
 
     public AboutView() {
         add(VaadinIcon.INFO_CIRCLE.create());
-        add(new Span(
-                " This application is using Vaadin version "
-                        + VaadinCoreShrinkWrap.class
-                                .getAnnotation(NpmPackage.class).version()
-                        + "."));
-
+        add(new Span(" This application is using Vaadin version "
+                + Version.getFullVersion() + "."));
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setAlignItems(Alignment.CENTER);
