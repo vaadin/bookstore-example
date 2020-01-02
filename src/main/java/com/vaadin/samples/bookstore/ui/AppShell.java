edcb0baef0830540290b83599008902188482f68
Manolo Carrasco <manolo@apache.org>
2020-01-02 10:48:40 +0100

    Update to use AppShellConfigurator

---
+++ src/main/java/com/vaadin/samples/bookstore/ui/AppShell.java
@@ -0,0 +1,9 @@
+package com.vaadin.samples.bookstore.ui;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+
+
+@PWA(name = "manolo", description = "manolito", shortName = "lolo")
+public class AppShell implements AppShellConfigurator {
+}
