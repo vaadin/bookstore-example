18a719b3904c96905333735efcd1066851727c87
Manolo Carrasco <manolo@apache.org>
2020-01-03 08:53:20 +0100

    CCDM: revert 8d41204, @PageTitle is allow in both app-shell and views

--- src/main/java/com/vaadin/samples/bookstore/ui/about/AboutView.java
+++ src/main/java/com/vaadin/samples/bookstore/ui/about/AboutView.java
@@ -3,11 +3,13 @@ package com.vaadin.samples.bookstore.ui.about;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.Version;
 import com.vaadin.samples.bookstore.ui.MainLayout;
 
 @Route(value = "About", layout = MainLayout.class)
+@PageTitle("About")
 public class AboutView extends HorizontalLayout {
 
     public static final String VIEW_NAME = "About";
