c466b1d4b22f8b843efb4471f1974e21ebf2922a
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-07-28 11:36:16 +0300

    Update RTL demo to 20.0.5

--- src/main/java/org/vaadin/example/bookstore/ui/MainLayout.java
+++ src/main/java/org/vaadin/example/bookstore/ui/MainLayout.java
@@ -35,7 +35,6 @@ import java.util.ResourceBundle;
 /**
  * The main layout. Contains the navigation menu.
  */
-@Theme(value = Lumo.class)
 @CssImport("./styles/shared-styles.css")
 @CssImport(value = "./styles/menu-buttons.css", themeFor = "vaadin-button")
 public class MainLayout extends AppLayout implements RouterLayout {
