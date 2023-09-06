0ac66a88f69ae9cfaaf47c256dd5a9354bd7f333
Manolo Carrasco <manolo@vaadin.com>
2023-09-06 13:03:12 +0200

    remove webdriver to use seleniun included one

--- src/test/java/org/vaadin/example/AbstractViewTest.java
+++ src/test/java/org/vaadin/example/AbstractViewTest.java
@@ -1,17 +1,17 @@
 package org.vaadin.example;
 
-import com.vaadin.flow.theme.AbstractTheme;
-import com.vaadin.testbench.ScreenshotOnFailureRule;
-import com.vaadin.testbench.TestBench;
-import com.vaadin.testbench.parallel.ParallelTest;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import com.vaadin.flow.theme.AbstractTheme;
+import com.vaadin.testbench.ScreenshotOnFailureRule;
+import com.vaadin.testbench.TestBench;
+import com.vaadin.testbench.parallel.ParallelTest;
 
 /**
  * Base class for ITs
