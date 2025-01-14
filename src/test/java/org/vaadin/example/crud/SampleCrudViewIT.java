d672702ff4fe9195dac73ca37412e28a7b38bd64
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2025-01-14 16:16:28 +0200

    Update SampleCrudViewIT.java

--- src/test/java/org/vaadin/example/crud/SampleCrudViewIT.java
+++ src/test/java/org/vaadin/example/crud/SampleCrudViewIT.java
@@ -17,6 +17,7 @@ public class SampleCrudViewIT extends AbstractViewTest {
     @Test
     public void userSelectsProduct_cannotEditProductInformation() {
 
+        waitUntil(driver->$(LoginFormElement.class).exists());
         // given authenticated as a regular user
         $(LoginFormElement.class).first().login("user", "user");
 
@@ -36,6 +37,7 @@ public class SampleCrudViewIT extends AbstractViewTest {
     @Test
     public void adminSelectsProduct_canUpdateProductInformation() {
 
+        waitUntil(driver->$(LoginFormElement.class).exists());
         // given authenticated as an admin
         $(LoginFormElement.class).first().login("admin", "admin");
 
@@ -60,6 +62,7 @@ public class SampleCrudViewIT extends AbstractViewTest {
 
     @Test
     public void adminCreatesNewProduct_productIsAvailableInGird() {
+        waitUntil(driver->$(LoginFormElement.class).exists());
         // given authenticated as an admin
         $(LoginFormElement.class).first().login("admin", "admin");
 
